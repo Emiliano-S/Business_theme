e5e5d17f0a3ee3b02efaef4039ce26dace27a726
Emiliano-S <97285075+Emiliano-S@users.noreply.github.com>
2022-03-07 18:22:03 +0100

    HomePage
    Creazione home page

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function mobileMenu(){
+    document.getElementById("headerMenu").classList.toggle('open');
+    document.getElementById("header").classList.toggle('open');
+    document.getElementById("mobileButtons").classList.toggle('open');
+}
